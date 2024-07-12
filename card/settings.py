7106630a225c168dcfdc5ff0ae3e218d07762fcc
s1mple088 <tursunovayubxon07@gmail.com>
2024-07-12 22:28:41 +0500

    update

--- card/settings.py
+++ card/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s=#zj^y0!9y*jklquw7=6ff_xp=7y00q4%ovu0n_r9wc!nckv&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.ngrok-free.app']
 
